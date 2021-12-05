e43e558f50bb17feedf0dda035f24debee718950
joan-kii <joan.sb81@gmail.com>
2021-12-05 06:16:51 +0100

    mueve index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import './src/index.css';
 
-import App from './App';
+import App from './src/App';
 
 ReactDOM.render(
   <React.StrictMode>
